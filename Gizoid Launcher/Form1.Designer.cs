39bf7347a8455955a0b93a0af214eb19345bbcf6
Oreboat <111535650+Oreboat@users.noreply.github.com>
2024-01-15 17:41:53 -0600

    Add project files.

---
+++ Gizoid Launcher/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Gizoid_Launcher
+{
+    partial class Gizoid_Launcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Gizoid_Launcher));
+            this.SuspendLayout();
+            // 
+            // Gizoid_Launcher
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.MinimizeBox = false;
+            this.Name = "Gizoid_Launcher";
+            this.Load += new System.EventHandler(this.Gizoid_Launcher_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Gizoid Launcher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gizoid_Launcher
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Console.WriteLine("Talk about a low budget flight, no food or movies! I'm outa here I like running better");
+            Application.Run(new Gizoid_Launcher());
+        }
+    }
+}
